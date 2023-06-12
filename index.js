bb89cb7ceca178b22f485016cdf5b37812f5a80a
apl09 <adrianpastorlopez09@gmail.com>
2023-06-12 16:07:32 +0200

    commit2

--- index.js
+++ index.js
@@ -336,3 +336,101 @@ const sentenceElements = [
    
 // Resultado--> 'Me llamo XX y quiero sentir la fuerza con javascript'
 
+//EXtra extraaa
+
+
+//Imprimir los números del 21 al 34 en la consola.
+
+const imprimir = array.map((a,b )=> a +b)
+
+
+//Sumar los números del 1 al 10 y mostrar el resultado en la consola.
+
+const sumar = array.reduce((a,b )=> a +b)
+
+//Dado un array de números, imprimir en la consola la suma de todos los números.
+
+const sumar2 = array.reduce((a,b )=> a +b)
+
+//Imprimir los números del 1 al 10 en la consola, pero solo los pares.
+
+const even = array.map(numero => {
+    if (numero %2 == 0){
+    return numero
+}
+
+}) 
+
+//Imprimir los números del 1 al 10 en la consola, pero solo los impares.
+
+const odd2 = array.map(numero => {
+    if (numero %2 !== 0){
+    return numero
+}
+
+}) 
+
+
+
+//Imprimir los números del 1 al 20 en la consola, pero para los múltiplos de 3 imprimir 
+//"Fizz", para los múltiplos de 5 imprimir "Buzz" y para los múltiplos de ambos imprimir "FizzBuzz".
+
+const fizzBuzz = array.map(numero => {
+    if ( numero %15 == 0  ){
+        return 'FizzBuzz'
+    } else if (numero %3 == 0){
+        return 'Fizz'
+    } else if (numero %5 == 0){
+        return 'Buzz'
+    } else return numero    
+})
+
+
+
+
+//Dado un array de números, imprimir en la consola solo los números pares.
+
+const even2 = array.map(numero => {
+    if (numero %2 == 0){
+    return numero
+}
+
+}) 
+
+
+
+
+//Dado un array de números, imprimir en la consola solo los números mayores a 5.
+const mayor5 = array.map(numero => {
+    if (numero > 5){
+    return numero
+}
+
+}) 
+
+
+
+
+//Dada una lista de nombres, imprimir solo los nombres que empiezan con la letra "A".
+const letraA = array.map(letra => {
+    if (letra.includes('A')){
+    return letra
+}
+
+}) 
+
+
+
+
+
+//Dada una lista de números, encontrar el número más grande y el número más pequeño.
+const numeroGrande = array.map(numero => {    
+    return Math.max(numero)
+
+}) 
+
+const numeroPequeño = array.map(numero => {    
+    return Math.min(numero)
+
+
+}) 
